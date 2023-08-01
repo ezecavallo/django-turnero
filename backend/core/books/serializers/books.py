68f65994d16ce6dcc28e4cda217e2eb7c4b34608
Ezequiel <ezecavallo@gmail.com>
2023-08-01 01:07:23 -0300

    Update Books serializer

--- backend/core/books/serializers/books.py
+++ backend/core/books/serializers/books.py
@@ -11,6 +11,7 @@ import uuid as uuuid
 class DurationSerializer(serializers.Serializer):
     uuid = serializers.UUIDField()
     duration = serializers.IntegerField()
+    price = serializers.IntegerField()
 
 
 class BookAdminSerializer(serializers.Serializer):
@@ -18,7 +19,6 @@ class BookAdminSerializer(serializers.Serializer):
 
     uuid = serializers.UUIDField(read_only=True)
     name = serializers.CharField(read_only=True)
-    price = serializers.IntegerField(read_only=True)
     deposit_percentage = serializers.IntegerField(read_only=True)
     durations = DurationSerializer(
         many=True,
@@ -37,8 +37,11 @@ class BookPublicSerializer(serializers.Serializer):
 
     uuid = serializers.UUIDField(read_only=True)
     name = serializers.CharField(read_only=True)
-    price = serializers.IntegerField(read_only=True)
     deposit_percentage = serializers.IntegerField(read_only=True)
+    durations = DurationSerializer(
+        many=True,
+        read_only=True,
+    )
 
     def create(self, validated_data):
         pass
@@ -51,7 +54,6 @@ class BookCreate(serializers.Serializer):
     """Book creation serializer"""
 
     name = serializers.CharField(max_length=150)
-    price = serializers.IntegerField()
     deposit_percentage = serializers.IntegerField(max_value=100, min_value=0)
     business = serializers.HiddenField(default='')
 
@@ -72,7 +74,6 @@ class BookUpdate(serializers.Serializer):
     """Book update serializer"""
 
     name = serializers.CharField(max_length=150)
-    price = serializers.IntegerField()
     deposit_percentage = serializers.IntegerField(max_value=100, min_value=0)
     durations = DurationSerializer(
         many=True

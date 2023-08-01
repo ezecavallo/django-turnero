386cf18641d949da208285aefee0eababbdb082b
Ezequiel <ezecavallo@gmail.com>
2023-08-01 01:06:36 -0300

    Update Duration model

--- backend/core/books/models/durations.py
+++ backend/core/books/models/durations.py
@@ -3,14 +3,20 @@
 # Django
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
 
 # Models
 from core.utils.models import BaseModel
 from core.books.models import Book
+from core.business.models import Business
 
 
 class DurationQuerySet(models.query.QuerySet):
     """Duration QuerySet"""
+
+    def filter_by_business(self, business: Business):
+        """Filter by business"""
+        return self.filter(book__business=business)
 
     def filter_by_book(self, book: Book):
         """Filter by book"""
@@ -32,6 +38,16 @@ class Duration(BaseModel):
         "books.Book",
         on_delete=models.CASCADE,
         related_name="durations"
+    )
+
+    price = models.DecimalField(
+        _("price"),
+        max_digits=24,
+        decimal_places=14,
+        help_text=_("The default price for events of this duration."),
+        validators=[
+            MinValueValidator(0)
+        ]
     )
 
     objects = DurationQuerySet.as_manager()
